ef1086fb30db223c40667ceaca8938852458e1ac
Nathalia <nathalia@Nathalias-MacBook-Air.local>
2022-02-16 13:19:25 -0300

    adding files

---
+++ urllib-counter.py
@@ -0,0 +1,13 @@
+from itertools import count
+import urllib.request, urllib.parse, urllib.error
+
+counts = dict()
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+    
+print(counts)
